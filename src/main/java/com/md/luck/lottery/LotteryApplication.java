dd910f289e06406f591cbd2ee03e4a17ea7502f4
unknown <madong@1m2.net>
2019-07-29 17:55:56 +0800

    Initial commit

---
+++ src/main/java/com/md/luck/lottery/LotteryApplication.java
@@ -0,0 +1,16 @@
+package com.md.luck.lottery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author madong
+ */
+@SpringBootApplication
+public class LotteryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LotteryApplication.class, args);
+    }
+
+}
