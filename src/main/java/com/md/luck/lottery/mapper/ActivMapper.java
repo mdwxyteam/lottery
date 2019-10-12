27587c7a9917fda56c2d12f20f4d219d13eba25d
madong <madong@1m2.net>
2019-10-12 10:32:43 +0800

    sql自动创建时间

--- src/main/java/com/md/luck/lottery/mapper/ActivMapper.java
+++ src/main/java/com/md/luck/lottery/mapper/ActivMapper.java
@@ -13,8 +13,8 @@ public interface ActivMapper {
      * @param activ activ
      * @return int
      */
-    @Insert("INSERT INTO lottery_activ (sponsorid, sponsor, location, address, condition_type, sponsor_claim, state, adv, condition)" +
+    @Insert("INSERT INTO lottery_activ (sponsorid, sponsor, location, address, condition_type, sponsor_claim, state, adv, condition, release_time)" +
             " VALUES (#{activ.sponsorid}, #{activ.sponsor}, #{activ.location}, #{activ.address}, #{activ.conditionType}" +
-            ", #{activ.sponsorClaim}, #{activ.state}, #{activ.adv}, #{activ.condition})")
+            ", #{activ.sponsorClaim}, #{activ.state}, #{activ.adv}, #{activ.condition}, now())")
     int add(@Param("activ") Activ activ);
 }
--- src/main/java/com/md/luck/lottery/mapper/SponsorMapper.java
+++ src/main/java/com/md/luck/lottery/mapper/SponsorMapper.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Mapper
 public interface SponsorMapper {
 
-    @Insert("INSERT INTO lottery_sponsor (sponsor, type, detalis, location, address, type_id) VALUES (#{sponsor.sponsorName},#{sponsor.type} ,#{sponsor.detalis} ,#{sponsor.location} , #{sponsor.address} ,#{sponsor.typeId})")
+    @Insert("INSERT INTO lottery_sponsor (sponsor, type, detalis, location, address, type_id, creat_time) VALUES (#{sponsor.sponsorName},#{sponsor.type} ,#{sponsor.detalis} ,#{sponsor.location} , #{sponsor.address} ,#{sponsor.typeId}), now()")
     int add(@Param("sponsor") Sponsor sponsor);
 
     @Select("SELECT * FROM lottery_sponsor")
